f520ec340cfa25b81630578da74e4efe101147ca
Can Koeksen <can.koeksen@gmail.com>
2021-08-08 00:07:53 +0200

    Add code to module instead of class

--- lib/two_thousand_forty_eight.rb
+++ lib/two_thousand_forty_eight.rb
@@ -1,36 +1,14 @@
 module TwoThousandFortyEight
+  extend self
+
+  @somethingMoved = 0
+  @points = 0
+
   def self.run
-    game = Game.new
+    start()
     true
   end
-end
-
-#-------------------------------------------------------------------------------------------------------------
-#To-do:
-#    Improve move functions & overall game logic               #Partially done
-
-# Already Done:
-#    Add basic game logic                                      #Done
-#    Add moveRight, moveLeft                                   #Done
-#    Add moveUp, moveDown                                      #Done
-#    Add function to create random numbers at random spots     #Done
-#    Add a basic UI for testing                                #Done
-#    Add user input                                            #Done
-#    Improve the UI                                            #Done
-#    Add win condition at 2048                                 #Done
-#    Reorganize Code                                           #Done
-#    Add a check if something moved                            #Done
-#    Add better classes                                        #Canceled
-#    Add a points counter                                      #Done
-
-
-class Game
-  def initialize()
-    @somethingMoved = 0
-    @points = 0
-    start()
-  end
-
+  
   def start()
     gridArray = Array.new(16,0)
     2.times do 
@@ -39,10 +17,29 @@ class Game
     routine(gridArray)
   end
 
-#-------------------------------------------------------------------------------------------------------------
-#Game Routine
+  #-------------------------------------------------------------------------------------------------------------
+  #To-do:
+  #    Improve move functions & overall game logic               #Partially done
 
-  def routine(grid)
+  # Already Done:
+  #    Add basic game logic                                      #Done
+  #    Add moveRight, moveLeft                                   #Done
+  #    Add moveUp, moveDown                                      #Done
+  #    Add function to create random numbers at random spots     #Done
+  #    Add a basic UI for testing                                #Done
+  #    Add user input                                            #Done
+  #    Improve the UI                                            #Done
+  #    Add win condition at 2048                                 #Done
+  #    Reorganize Code                                           #Done
+  #    Add a check if something moved                            #Done
+  #    Add better classes                                        #Canceled
+  #    Add a points counter                                      #Done
+
+
+  #-------------------------------------------------------------------------------------------------------------
+  #Game Routine
+
+  def self.routine(grid)
     while true
       @somethingMoved = 0
       drawInterface(grid)
@@ -359,138 +356,6 @@ class Game
     return grid
   end
 
-  #Maybe in the future
-  def LRshift(grid, direction)
-    rowCounter = 0
-    4.times do
-      alreadyMerged = Array.new(16,0)
-      tilePosition = direction == "d" ? 2+rowCounter : 1+rowCounter
-      maxTile = direction == "d" ? -1+rowCounter : 4+rowCounter
-      lastPosition = direction == "d" ? 3+rowCounter : rowCounter
-
-      until tilePosition == maxTile do
-
-        moveToCounter = 1
-        4.times do
-
-          moveToPosition = direction == "d" ? tilePosition + moveToCounter : tilePosition - moveToCounter
-          if moveToPosition > 15 then moveToPosition = 15 end
-          if moveToPosition < 0 then moveToPosition = 0 end
-          tileInfront = direction == "d" ? moveToPosition-1 : moveToPosition+1
-          
-
-          if grid[moveToPosition] > 0                                 #If field is not empty
-            if grid[tilePosition] == grid[moveToPosition]             #If both fields contain the same number
-              if alreadyMerged[moveToPosition] == 0                   #If the tile (where to move) has not merged yet
-                grid = merge(grid, tilePosition, moveToPosition)
-                @points = @points+grid[tilePosition]+grid[moveToPosition]
-                alreadyMerged[moveToPosition],@somethingMoved = 1, 1
-                break
-              elsif alreadyMerged[moveToPosition] == 1
-                if tileInfront != tilePosition                          #if Tile infront is not the same as tilePosition
-                  grid = moveInfront(grid, tilePosition, tileInfront)
-                  @somethingMoved = 1
-                end
-                break
-              end            
-            else
-              if grid[moveToPosition] != 0
-                if tileInfront != tilePosition
-                  if tilePosition != 0
-                    grid = moveInfront(grid, tilePosition, tileInfront)
-                    @somethingMoved = 1
-                  end
-                end
-              end
-              break
-            end
-          end
-          moveToCounter += 1
-          if moveToCounter == 4
-            if tilePosition != 0
-              grid = moveToBack(grid,tilePosition, lastPosition)
-              @somethingMoved = 1
-              break
-            end
-          end
-        end
-        if tilePosition != maxTile
-          tilePosition = direction == "d" ? tilePosition-1 : tilePosition+1
-        end
-      end
-      if rowCounter != 12
-        rowCounter += 4
-      end
-    end
-    return grid
-  end 
-
-  def UDshift(grid, direction)
-    columnCounter = 0
-    4.times do
-      alreadyMerged = Array.new(16,0)
-      tilePosition = direction == "s" ? 8+columnCounter : 4+columnCounter
-      maxTile = direction == "s" ? -4+columnCounter : 16+columnCounter
-      lastPosition = direction == "s" ? 12+columnCounter : columnCounter
-
-      until tilePosition == maxTile do
-
-        moveToCounter = 4
-        4.times do
-
-          moveToPosition = direction == "s" ? tilePosition + moveToCounter : tilePosition - moveToCounter
-          if moveToPosition >= 16 && columnCounter == 0 then moveToPosition = 12 end    
-          if moveToPosition >= 16 && columnCounter == 1 then moveToPosition = 13 end
-          if moveToPosition >= 16 && columnCounter == 2 then moveToPosition = 14 end
-          if moveToPosition >= 16 && columnCounter == 3 then moveToPosition = 15 end
-          tileInfront = direction == "s" ? moveToPosition-4 : moveToPosition+4
-          
-
-          if grid[moveToPosition] > 0                                 #If field is not empty
-            if grid[tilePosition] == grid[moveToPosition]             #If both fields contain the same number
-              if alreadyMerged[moveToPosition] == 0                   #If the tile (where to move) has not merged yet
-                grid = merge(grid, tilePosition, moveToPosition)
-                @points = @points+grid[tilePosition]+grid[moveToPosition]
-                alreadyMerged[moveToPosition],@somethingMoved = 1, 1
-                break
-
-              elsif alreadyMerged[moveToPosition] == 1
-                if tileInfront != tilePosition                          #if Tile infront is not the same as tilePosition
-                  grid = moveInfront(grid, tilePosition, tileInfront)
-                  @somethingMoved = 1
-                end
-                break
-              end            
-            else
-              if tileInfront != tilePosition                                #If tile infront is not the same as current tile
-                if grid[tilePosition] != 0                                  #If current tile is not empty
-                  grid = moveInfront(grid, tilePosition, tileInfront)
-                  @somethingMoved = 1
-                end
-              end
-              break
-            end
-          end
-          moveToCounter += 4
-          if moveToCounter == 16
-            if tilePosition != 0
-              grid = moveToBack(grid,tilePosition, lastPosition)
-              @somethingMoved = 1
-              break
-            end
-          end
-        end
-        if tilePosition >= columnCounter
-          tilePosition = direction == "s" ? tilePosition-4 : tilePosition+4
-        end
-      end
-      if columnCounter != 4
-        columnCounter += 1
-      end
-    end
-    return grid
-  end 
-
   #Help functions
   def generateRandomInt()
     #Generates either 2 or 4 | 90%/10%
@@ -579,7 +444,7 @@ class Game
   #User Interface
   def drawInterface(grid)
     #Clears the console, prints points and the grid
-    #system "clear"
+    system "clear"
     print("\nPoints: ", @points, "\n\n")   
     counter = 0
 
