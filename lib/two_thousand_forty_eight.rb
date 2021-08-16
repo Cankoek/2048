3ac1435db32731b94e11a8f7c5a8b46d21d91ae9
Can Koeksen <can.koeksen@gmail.com>
2021-08-16 18:22:43 +0200

    Start to rework shift function

--- lib/two_thousand_forty_eight.rb
+++ lib/two_thousand_forty_eight.rb
@@ -1,7 +1,13 @@
 require "io/console"
-MOVE_LEFT = 'a'
 module TwoThousandFortyEight
   extend self
+
+  MOVE_RIGHT = 'd'
+  MOVE_LEFT = 'a'
+  MOVE_UP = 'w'
+  MOVE_DOWN = 's'
+  YES = 'y'
+  NO = 'n'
 
   @somethingMoved = 0
   @points = 0
@@ -13,12 +19,13 @@ module TwoThousandFortyEight
     system "clear"
     grid = Array.new(16,0)
     2.times do gridArray = addRandomNumber(grid) end
+
     while true
       #Check win
       if @alreadyWon == 0 && won?(grid)
         drawInterface(grid)
         print("\nCongratulations! You've beat 2048.\nDo you want to continue? (y:Yes n:No)\n")
-        if userInput("yn").downcase == 'n' then exit() end
+        exit() if userInput("yn").downcase == 'n'
       end
 
       #Check lose
@@ -29,10 +36,10 @@ module TwoThousandFortyEight
         else exit() end
       end
 
-
       drawInterface(grid)
       puts("\nw:Up a:Left s:Down d:Right r:Restart e:End\n")
-      @somethingMoved, direction = 0, userInput("wasder")
+      @somethingMoved = 0
+      direction = userInput("wasder")
 
       #Check input
       grid = shift(grid,direction) if "wasd".include? direction.downcase
@@ -42,7 +49,7 @@ module TwoThousandFortyEight
       elsif direction == 'e' then exit end
 
       #If anything moved, adds a new random number to the grid
-      if @somethingMoved == 1 then grid = addRandomNumber(grid) end           
+      addRandomNumber(grid) if @somethingMoved == 1
       sleep(0.01)
     end
     true
@@ -55,90 +62,79 @@ module TwoThousandFortyEight
     counter = 0
     4.times do
       alreadyMerged = Array.new(16,0)
-      if direction.downcase == 'd'
+      case direction.downcase
+      when MOVE_RIGHT
         @lastMove = 'Right'
         tilePosition = 2+counter
         maxTile = -1+counter
         lastPosition = 3+counter
-      elsif direction.downcase == 'a'
+      when MOVE_LEFT
         @lastMove = 'Left'
         tilePosition = 1+counter
         maxTile = 4+counter
         lastPosition = counter
-      elsif direction.downcase == 's'
-        @lastMove = 'Down'
-        tilePosition = 8+counter
-        maxTile = -4+counter
-        lastPosition = 12 + counter
-      elsif direction.downcase == 'w'
+      when MOVE_UP
         @lastMove = 'Up'
         tilePosition = 4+counter
         maxTile = 16+counter
         lastPosition = counter
+      when MOVE_DOWN
+        @lastMove = 'Down'
+        tilePosition = 8+counter
+        maxTile = -4+counter
+        lastPosition = 12 + counter
       end
 
       until tilePosition == maxTile do
 
-        if direction.downcase == 'a' || direction.downcase == 'd' then moveToCounter = 1 end
-        if direction.downcase =='s' || direction.downcase == 'w' then moveToCounter = 4 end
+        moveToCounter = 1 if direction.downcase == MOVE_RIGHT || direction.downcase == MOVE_LEFT
+        moveToCounter = 4 if direction.downcase == MOVE_UP || direction.downcase == MOVE_DOWN
 
         4.times do
-          if direction.downcase == 'd'
+          if direction.downcase == MOVE_RIGHT
             moveToPosition = tilePosition+moveToCounter
             tileInfront = moveToPosition-1
-          elsif direction.downcase == 'a'
+          elsif direction.downcase == MOVE_LEFT
             moveToPosition = tilePosition-moveToCounter
             tileInfront = moveToPosition+1
-          elsif direction.downcase == 's'
+          elsif direction.downcase == MOVE_DOWN
             moveToPosition = tilePosition+moveToCounter
             tileInfront = moveToPosition-4
-          elsif direction.downcase == 'w'
+          elsif direction.downcase == MOVE_UP
             moveToPosition = tilePosition-moveToCounter
             tileInfront = moveToPosition+4
           end
-          if moveToPosition > 15 then moveToPosition = 15 end
+          moveToPosition = 15 if moveToPosition > 15
 
-
-          if grid[moveToPosition] > 0                                       #If desired tile is not empty
-            if grid[moveToPosition] == grid[tilePosition]                   #If tile and desired tile contain the same number
-              if alreadyMerged[moveToPosition] == 0                         #If desired tile did not merge yet this round
+          if grid[moveToPosition] > 0                                       #
+            if grid[moveToPosition] == grid[tilePosition]   
+              case alreadyMerged[moveToPosition]
+              when 0
                 grid = merge(grid, tilePosition, moveToPosition)            
                 alreadyMerged[moveToPosition],@somethingMoved = 1, 1
                 break 
-              else
-                if alreadyMerged[moveToPosition] == 1                       #Else If desired tile already merged this round
-                  if tileInfront != tilePosition 
-                    if grid[tilePosition] != 0                              #If tile infront is not the same as current tile
-                      grid = moveInfront(grid, tilePosition, tileInfront)
-                      @somethingMoved = 1
-                    end
-                  end
-                  break
-                end
+              when 1
+                grid = moveInfront(grid, tilePosition, tileInfront)
+                break             
               end
             else
-              if tileInfront != tilePosition                                #If tile infront is not the same as current tile
-                if grid[tilePosition] != 0                                  #If current tile is not empty
-                  grid = moveInfront(grid, tilePosition, tileInfront)
-                  @somethingMoved = 1
-                end
-              end
-              break
+              grid = moveInfront(grid, tilePosition, tileInfront)
+              break 
             end
           end
-          if direction.downcase == 'a' || direction.downcase == 'd'  
+          if direction.downcase == MOVE_LEFT || direction.downcase == MOVE_RIGHT  
             moveToCounter += 1
-            if moveToCounter == 4                                             #If nothing happened 4 times in a row
-              if grid[tilePosition] != 0                                      #If current tile is not empty
+            if moveToCounter == 4                                             
+              if grid[tilePosition] != 0                                      
                 moveToBack(grid, tilePosition,lastPosition)
                 @somethingMoved = 1
                 break
               end
             end
-          elsif direction.downcase == 's' || direction.downcase == 'w'
+          elsif direction.downcase == MOVE_DOWN || direction.downcase == MOVE_UP
             moveToCounter += 4
-            if moveToCounter == 16                                            #If nothing happened 4 times in a row
-              if grid[tilePosition] != 0                                      #If current tile is not empty
+            if moveToCounter == 16                                            
+              if grid[tilePosition] != 0                                     
                 moveToBack(grid, tilePosition,lastPosition)
                 @somethingMoved = 1
                 break
@@ -146,28 +142,35 @@ module TwoThousandFortyEight
             end
           end
         end
-        if direction.downcase == 'a' || direction.downcase == 'd'
-          if tilePosition != maxTile
-            if direction.downcase == 'd' then tilePosition -= 1 end
-            if direction.downcase == 'a' then tilePosition += 1 end
-          end
-        else
-          if tilePosition >= counter
-            if direction.downcase == 's' then tilePosition -= 4 end
-            if direction.downcase == 'w' then tilePosition += 4 end
-          end
-        end
+
+        tilePosition = retTilePosition(direction,tilePosition,maxTile,counter)
       end
 
-      if direction.downcase == 'a' || direction.downcase == 'd'
-        if counter != 12 then counter += 4 end
-      elsif direction.downcase == 's' || direction.downcase == 'w'
-        if counter != 4 then counter += 1 end
+      case direction.downcase
+      when MOVE_RIGHT,MOVE_LEFT
+        counter += 4 if counter != 12
+      when MOVE_UP,MOVE_DOWN
+        counter += 1 if counter != 4
       end
 
     end
     return grid
   end
+
+  def retTilePosition(direction,tilePosition,maxTile,counter)
+    case direction.downcase
+    when MOVE_RIGHT 
+      tilePosition -= 1 if tilePosition != maxTile
+    when MOVE_LEFT
+      tilePosition += 1 if tilePosition != maxTile
+    when MOVE_DOWN 
+      tilePosition -= 4 if tilePosition >= counter
+    when MOVE_UP
+      tilePosition += 4 if tilePosition >= counter
+    end
+  end
+
+
 
   #Move functions
   def merge(grid,tilePosition, moveToPosition)
@@ -179,9 +182,14 @@ module TwoThousandFortyEight
   end
 
   def moveInfront(grid, tilePosition, tileInfront)
-    #Move current tile infront of desired tile
-    grid[tileInfront] = grid[tilePosition] 
-    grid[tilePosition] = 0
+    if tileInfront != tilePosition 
+      if grid[tilePosition] != 0   
+        #Move current tile infront of desired tile
+        grid[tileInfront] = grid[tilePosition] 
+        grid[tilePosition] = 0
+        @somethingMoved = 1
+      end
+    end
     return grid
   end
 
@@ -212,7 +220,8 @@ module TwoThousandFortyEight
     while true
       input = STDIN.getch
       #Checks if input is in inputrange
-      if inputrange.include? input.downcase then break else puts("Invalid Input. Try again.") end
+      if inputrange.include? input.downcase then break 
+      else puts("Invalid Input. Try again.") end
     end
     return input
   end
@@ -235,22 +244,22 @@ module TwoThousandFortyEight
     for counter in (0..15) do
       if counter < 15 && counter != 3 && counter != 7 && counter != 11
         plusone = counter + 1
-        if grid[plusone] == grid[counter] then return false end
+        return false if grid[plusone] == grid[counter]
       end
 
       if counter > 0 && counter != 4 && counter != 8 && counter != 12
         minusone = counter - 1
-        if grid[minusone] == grid[counter] then return false end
+        return false if grid[minusone] == grid[counter]
       end
 
       if counter < 12
         plusfour = counter + 4
-        if grid[plusfour] == grid[counter] then return false end
+        return false if grid[plusfour] == grid[counter]
       end
 
       if counter > 3
-        minusfour = counter + 4
-        if grid[minusfour] == grid[counter] then return false end
+        minusfour = counter - 4
+        return false if grid[minusfour] == grid[counter]
       end
     end
     return true
@@ -258,7 +267,8 @@ module TwoThousandFortyEight
 
   def reset()
     #Resets points and starts again
-    @alreadyWon,@points = 0, 0
+    @alreadyWon = 0
+    @points = 0
     run()
   end
 
